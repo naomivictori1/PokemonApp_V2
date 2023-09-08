13476041d737b8270cd9b23015de5cdf779ecc54
Naomi Victoria Obame <71739822+naomivictori1@users.noreply.github.com>
2023-09-07 21:21:14 -0400

    modified files

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,27 +1,42 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Pagination from './Pagination';
 
 export default function Homepage() {
-  const [pokemonlist, setPokemonlist] = useState([]);
+	const [pokemonlist, setPokemonlist] = useState([]);
+	const [currentPageNumber, setCurrentPageNumber] = useState(1);
+	const [pokemonPerPage] = useState(100);
 
-  useEffect(() => {
-    axios
-      .get("https://pokeapi.co/api/v2/pokemon/")
-      .then((data) => setPokemonlist(data.data.results))
-      .catch((e) => console.log(e));
-  }, []);
+	useEffect(() => {
+		axios
+			.get('https://pokeapi.co/api/v2/pokemon?limit=1279')
+			.then((data) => setPokemonlist(data.data.results))
+			.catch((e) => console.log(e));
+	}, []);
 
-  return (
-    <>
-      <h1>Header</h1>
-      <ul>
-        {pokemonlist.map((pokemon) => (
-          <li key={pokemon.name}>
-            <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+	const indexOfLastPokemon = currentPageNumber * pokemonPerPage;
+	const indexOfFirstPokemon = indexOfLastPokemon - pokemonPerPage;
+	const currentPokemon = pokemonlist.slice(
+		indexOfFirstPokemon,
+		indexOfLastPokemon,
+	);
+
+	const paginate = (pageNumber) => setCurrentPageNumber(pageNumber);
+	return (
+		<>
+			<ul className="pokelist">
+				{currentPokemon.map((pokemon) => (
+					<h2 key={pokemon.name}>
+						<Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
+					</h2>
+				))}
+			</ul>
+			<Pagination
+				postsPerPage={pokemonPerPage}
+				totalPosts={pokemonlist.length}
+				paginate={paginate}
+			/>
+		</>
+	);
 }
